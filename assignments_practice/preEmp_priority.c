d6dca2e6831b02d0094024b5ce1e4c3b12a1a3a8
theskylighter <2023uai1819@mnit.ac.in>
2024-11-27 11:19:31 +0530

    practicing

---
+++ assignments_practice/preEmp_priority.c
@@ -0,0 +1,13 @@
+// while( completed <n ) 
+	// use variable -> highest priority index
+	
+	// increment time ++;
+	
+	// check and update current highest priority
+	
+	// decrement RemTime --; 
+	
+	// if finished
+		// calculate WT[i] & TAT[i] 	
+
+// print the solution 
